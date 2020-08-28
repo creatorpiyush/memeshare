f4e9a99c51fd20d29cde2610937da5798e89aa59
creatorpiyush <piyushanand.1221@gmail.com>
2020-08-29 00:36:29 +0530

    modal button redirected

--- src/public/app/common.js
+++ src/public/app/common.js
@@ -1,5 +1,6 @@
 $(() => {
   $("#navbar").load("/components/navbar.html");
   $("#cards").load("/components/cards.hbs");
+  $("#addmeme").load("/components/addmeme.hbs");
   $("#footer").load("/components/footer.html");
 });
