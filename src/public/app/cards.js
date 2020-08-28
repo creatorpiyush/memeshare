9dd258d488af7568d820b7270db48afa56f08db7
creatorpiyush <piyushanand.1221@gmail.com>
2020-08-28 22:49:22 +0530

    online picture storage added using Cloudinary

--- src/public/app/cards.js
+++ src/public/app/cards.js
@@ -3,8 +3,8 @@ function loadProducts() {
     for (let m of meme) {
       $("#posts-container").append(
         $(`<div class="card-image">
-                <img src="..${m.memeimg}">
-                    <a href="..${m.memeimg}" class="btn-floating halfway-fab waves-effect waves-light blue" style="font-size: 35px;" download="${m.memetitle}"> 
+                <img src="${m.memeimg}">
+                    <a href="${m.memeimg}" class="btn-floating halfway-fab waves-effect waves-light blue" style="font-size: 35px;" download="${m.memetitle}"> 
                     <i class="material-icons">get_app</i></a>
             </div>
             <span class="card-title" style="color: black; font-weight: 1000;">${m.memetitle}</span>
--- src/routes/cardsdb.js
+++ src/routes/cardsdb.js
@@ -3,51 +3,58 @@ const route = express.Router();
 const multer = require("multer");
 const fs = require("fs").promises;
 
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name: "creatorpiyush",
+  api_key: "897718564747982",
+  api_secret: "J8UTV3thq628g70AC3R-eXDG5sw",
+});
+
 const addmeme = require("../db/db");
+const { result } = require("lodash");
 
 // * images upload
 const upload = multer({ dest: "src/uploads/" });
 
 route.use("/images", express.static("../images"));
+
+// * first page
 route.get("/", (req, res) => {
   res.render("addmeme");
 });
 route.post("/add", upload.single("memeimg"), (req, res) => {
   // console.log("req.body", req.body);
-  // console.log("req.file", req.file);
-  // console.log("req.file", req.file.filename);
-  if (req.body.memetitle === "" || req.body.memetitle == null) {
-    return res
-      .status(422)
-      .render("addmeme", { err: "Please Add a Meme Title" });
-  }
-  if (req.file == undefined) {
-    return res.status(422).render(".", { err: "Please Add a Meme" });
-  }
+  console.log("req.file", req.file);
 
-  const oldPath = "src/uploads/" + req.file.filename;
+  cloudinary.uploader.upload(req.file.path, (err, result) => {
+    console.log("error: ", err);
+    console.log("result:", result);
 
-  const newPath =
-    "src/public/images/" +
-    "memeimg_" +
-    Date().split(" ").join("_").split(":").join("_") +
-    "." +
-    req.file.mimetype.split("/").pop();
-
-  fs.rename(oldPath, newPath);
-
-  const temp = new addmeme({
-    memeimg: newPath.replace("src/public", ""),
-    memeid: Date().split(" ").join("_").split(":").join("_"),
-    memetitle: req.body.memetitle,
-  });
-
-  temp.save((err, result) => {
-    if (err) {
-      return res.send(err);
+    if (req.body.memetitle === "" || req.body.memetitle == null) {
+      return res
+        .status(422)
+        .render("addmeme", { err: "Please Add a Meme Title" });
     }
-    return res.redirect("/");
+    if (req.file == undefined) {
+      return res.status(422).render(".", { err: "Please Add a Meme" });
+    }
+
+    const temp = new addmeme({
+      memeimg: result.url,
+      memeid: Date().split(" ").join("_").split(":").join("_"),
+      memetitle: req.body.memetitle,
+    });
+
+    temp.save((err, result) => {
+      if (err) {
+        return res.send(err);
+      }
+      return res.redirect("/");
+    });
   });
+
+  // console.log("req.file", req.file.filename);
 });
 
 module.exports = route;
